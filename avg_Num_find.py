bfed61a42ccec85e88baa96972f5c92324a6feed
Laspric <Laspric>
2023-05-06 16:29:08 +0500

    Find Average Number Given By User

---
+++ avg_Num_find.py
@@ -0,0 +1,10 @@
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+d = int(input("Enter Fourth Number: "))
+e = int(input("Enter Fifth Number: "))
+
+add = a + b + c + d + e
+print("Addition is ", add)
+avg = add / 5
+print(avg)
